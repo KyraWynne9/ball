d0496ab72c887092798328ff2282646d34eb8214
KyraWynne9 <kewynne@email.neit.edu>
2026-04-01 10:54:34 -0400

    a

--- ball.js
+++ ball.js
@@ -1,18 +1,29 @@
 const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
 
+
 let x = canvas.width / 2;
 let y = canvas.height / 2;
 const radius = 30;
 let dx = 6;
-let dy = -6;
+let dy = 6;
 
 function Ball() {
 ctx.beginPath();
 ctx.arc(x, y, radius, 0, Math.PI * 2);
-ctx.fillStyle = "purple";
+ctx.fillStyle = this.color;
 ctx.fill();
 ctx.closePath();
+}
+
+function getRandomColor() {
+
+  var letter = "0123456789ABCDEF".split(""); 
+  var color = "#";                           
+  for(var i = 0; i < 6; i++){                
+    color += letter[Math.round(Math.random()*15)]; 
+  }
+  return color;
 }
 
 function draw() {
@@ -23,8 +34,15 @@ Ball();
 x += dx;
 y += dy;
 
-if (x + radius > canvas.width || x - radius < 0) dx = -dx;
-if (y + radius > canvas.height || y - radius < 0) dy = -dy;
+if (x + radius > canvas.width || x - radius < 0 ) {
+    color = getRandomColor();
+    dx = -dx;
+} 
+if (y + radius > canvas.height || y - radius < 0) {
+    color = getRandomColor();
+    dy = -dy;
+} 
+
 
 requestAnimationFrame(draw);
 }
