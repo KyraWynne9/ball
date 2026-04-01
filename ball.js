e8df1d58cde7b5f0faa646b4e8aa69be2210cb02
KyraWynne9 <kewynne@email.neit.edu>
2026-03-31 22:35:00 -0400

    a

--- ball.js
+++ ball.js
@@ -1,16 +1,29 @@
 const myCanvas = document.getElementById("myCanvas");
 const ctx = myCanvas.getContext("2d");
 
+let x = myCanvas.width / 2;
+let y = myCanvas.height -2;
+let dx = 2;
+let dy = -2;
+
 function Ball() {
-ctx.y = "center";
-ctx.x = "center";
-ctx.width = "10";
-ctx.height = "10";
-ctx.force = "0";
 ctx.beginPath();
 ctx.arc(100, 75, 50, 0, 2 * Math.PI);
 ctx.fillStyle = "purple";
 ctx.fill();
+ctx.closePath();
+x += dx;
+y += dy;
 }
 
 var ball = new Ball();
+
+var mainTimer = setInterval(animate, 1000/60);
+
+function animate()
+{
+ctx.clearRect(0, 0, myCanvas.width, myCanvas.height);
+Ball();
+x += dx;
+y += dy;
+}
